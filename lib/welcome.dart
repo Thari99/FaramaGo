77a6c90c40ca137eb36e6bdaa01ac3714823297c
Thari99 <64419836+Thari99@users.noreply.github.com>
2022-03-20 17:47:10 +0530

    ui design

---
+++ lib/welcome.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Welcomepage(),
+  ));
+}
+
+class Welcomepage extends StatelessWidget {
+  const Welcomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(140, 166, 186, 1.0),
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 45,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ],
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/welcome-page/farmer.png'))),
+                ),
+                const SizedBox(
+                  height: 75,
+                ),
+                Column(children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Text(
+                      "Farmer",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Text(
+                      "Bayer",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ));
+  }
+}
